b4eec383a5d1f1e89cd20c39d7e72ab8d7a7830b
Rob Koch <kochrt@gmail.com>
2023-08-10 12:57:19 -0400

    lpc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,43 @@
 import { OneView } from "./oneview/oneview";
 import { useLpc } from "@markwhen/view-client";
 import { isEventNode, walk2 } from "@markwhen/parser/lib/Noder";
+import { Path } from "@markwhen/parser/lib/Types";
 
 function bob() {
-  OneView.core = new OneView.Core();
-  OneView.core.init();
-  OneView.core.startDrawLoop();
+
+  let hovering: Path | undefined;
   let isDark = false;
+  let events: OneView.CalendarEventObject[] = [];
+
+  const redraw = (
+    events: OneView.CalendarEventObject[],
+    hovering: Path | undefined
+  ) => {
+    OneView.core.calendarEventHandler.clearAllEvents();
+    OneView.core.calendars = [
+      new OneView.CalendarObject(
+        "Default",
+        "default",
+        [],
+        0,
+        OneView.VisibilityType.Visible,
+        true,
+        true
+      ),
+    ];
+    OneView.core.calendarPrimaryId = "Default";
+    OneView.core.getCalendar("Default").allEventsAreFullDay = false;
+    OneView.core.calendarEventHandler.gradeCalendarEvents(events);
+    events.sort(function (a, b) {
+      return a.startZOP - b.startZOP;
+    });
+    events.forEach((e) => {
+      e.hovering = e.eventId === hovering?.join(",");
+      OneView.core.calendarEventHandler.addEventToCalendar(e);
+    });
+    OneView.core.calendarEventHandler.findCommonTimes();
+    OneView.core.redraw(true);
+  };
 
   const { postRequest } = useLpc({
     appState(appState) {
@@ -14,25 +45,20 @@ function bob() {
         isDark = !!appState.isDark;
         OneView.core.commonUserSettings.theme = isDark ? "3" : "0";
         OneView.core.settings.reloadTheme();
-        OneView.core.redraw(false)
+        OneView.core.redraw(false);
+      }
+      if (
+        (!appState.hoveringPath && !hovering) ||
+        appState.hoveringPath?.join(",") === hovering?.join(",")
+      ) {
+        // do nothing, they are equivalent
+      } else {
+        hovering = appState.hoveringPath;
+        redraw(events, hovering);
       }
     },
     markwhenState(markwhenState) {
-      OneView.core.calendarEventHandler.clearAllEvents();
-      OneView.core.calendars = [
-        new OneView.CalendarObject(
-          "Default",
-          "default",
-          [],
-          0,
-          OneView.VisibilityType.Visible,
-          true,
-          true
-        ),
-      ];
-      OneView.core.calendarPrimaryId = "Default";
-      OneView.core.getCalendar("Default").allEventsAreFullDay = false;
-      const events = [];
+      events = [];
       for (const { node, path } of walk2(markwhenState.transformed, [])) {
         if (node && isEventNode(node)) {
           const eventObj = new OneView.CalendarEventObject(
@@ -47,15 +73,13 @@ function bob() {
           events.push(eventObj);
         }
       }
-      OneView.core.calendarEventHandler.gradeCalendarEvents(events);
-      events.sort(function (a, b) {
-        return a.startZOP - b.startZOP;
-      });
-      events.forEach(e => OneView.core.calendarEventHandler.addEventToCalendar(e));
-      OneView.core.calendarEventHandler.findCommonTimes();
-      OneView.core.redraw(true);
+      redraw(events, hovering);
     },
   });
+
+  OneView.core = new OneView.Core(postRequest);
+  OneView.core.init();
+  OneView.core.startDrawLoop();
 
   postRequest("appState");
   postRequest("markwhenState");
@@ -68,12 +92,4 @@ window.onload = function () {
       console.error(a);
     }
   }, 50);
-};
-window.onpopstate = function (a) {
-  OneView.core &&
-    OneView.core.appStateHandler &&
-    OneView.core.appStateHandler.back();
-};
-window.onhashchange = function () {
-  OneView.core.eventHandler.closeAllPagesAndMenus();
 };
