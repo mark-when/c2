e8c12090b0ebc2fc5c63eaef00c0122800cf7243
Rob Koch <kochrt@gmail.com>
2024-04-19 18:47:38 -0700

    single file output

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [viteSingleFile()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
